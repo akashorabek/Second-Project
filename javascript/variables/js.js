898a166016b7516594b379a15a3b8c655c0faf06
Aka <aka.shorabek01@gmail.com>
2020-09-24 18:04:40 +0600

    first commit

---
+++ javascript/variables/js.js
@@ -0,0 +1,126 @@
+
+
+//Математические значение
+
+// let num1 = Math.pow(4, 5); //Стпень
+// let num2 = Math.sqrt(num1) - 0.6; //Корень
+// console.log(Math.round(3.5)); //Окургление
+// console.log(Math.ceil(241.2)); //Окургление на большую цифру
+// console.log(Math.floor(num2)); //Окургление на меньшую цифру
+// console.log(Math.floor(Math.random() * 1000)); //Рандом.орг
+
+// let a = 6;
+// let b = 8;
+
+// let c = Math.sqrt(Math.pow(a, 2) + Math.pow(b, 2));
+// console.log(c);
+
+
+
+
+// Conditions Условные операторы
+
+//&& 
+
+// let n1 = Number(prompt("Enter your number 1"))
+// let n2 = Number(prompt("Enter your number 2"))
+
+// if (n1 > n2){
+//     console.log(n1);
+// } else if (n1 == n2) {
+//     console.log("Your numbers are same");
+// } else {
+//     console.log(n2);
+// }
+
+
+// const age = +prompt("Сколько вам лет?");
+
+// if (age > 18 || age == 18) {
+//     alert("Добро Пожаловать.")
+// } else {
+//     alert("Вам запрещено посещение этого сайта!!!")
+// }
+
+// const name = prompt("Как ваша Имя?");
+// const surname = prompt("Как ваша Фамилия?");
+// const age = confirm("Вам есть 18 лет?");
+
+// let data = [name, surname, age];
+// if (2 == true) {
+//     alert("Добро Пожаловать," + name + " " + surname +".");
+// } else {
+//     alert("Извини," + name +".Ты не можешь посещать этот сайт.");
+// }
+
+// let a = Number(prompt("Сколько типов данных в JS?"));
+// let b = confirm("Html, css являются языками программирование?");
+// let c = Number(prompt(""));
+
+// if(a == 3 && b == false && c <= 2) {
+//     alert("");
+// } else if(a == 3 && b == false && c > 2) {
+//     alert("");
+// } else {
+//     alert("");
+// }
+
+// Домашка
+
+// let num = Number(prompt("Введите порядковый номер пальца"));
+
+// if (num == 1) {
+//     alert("Большой палец");
+// } else if(num == 2) {
+//     alert("Указательный палец");
+// } else if(num == 3) {
+//     alert("Среднйи палец");
+// } else if(num == 4) {
+//     alert("Безымянный палец");
+// } else if(num == 5) {
+//     alert("Мизинец");
+// } else {
+//     alert("Такого пальца нет")
+// }
+
+
+// let num1 = Number(Math.round(prompt("Укажите число #1")));
+// let num2 = Number(Math.round(prompt("Укажите число #2")));
+// let result = num1 % num2;
+
+// if (num1 % num2 == 0) {
+//     alert("Делиться")
+// } else {
+//     alert("Не делиться, остаток: "+ result)
+// }
+
+// let num1 = Number(prompt("Укажите число #1"));
+// let num2 = Number(prompt("Укажите число #2"));
+// let result = num1 + num2;
+
+// if (num1 < 0 || num2 < 0) {
+//     alert("Условие не выполняется")
+// } else {
+//     alert("Сумма: "+ result)
+// }
+
+// let num1 = Number(prompt("Укажите число #1"));
+// let num2 = Number(prompt("Укажите число #2"));
+// let num3 = Number(prompt("Укажите число #3"));
+
+// if (num1 % 5 == 0) {
+//     console.log(num1);
+// }
+// if (num2 % 5 == 0) {
+//     console.log(num2);
+// }
+// if (num3 % 5 == 0) {
+//     console.log(num3);
+// }
+
+
+let num1 = Number(prompt("Введите число 1"))
+let num2 = Number(prompt("Введите число 2"))
+let num3 = Number(prompt("Введите число 3"))
+let num4 = Number(prompt("Введите число 4"))
+
